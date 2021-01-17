97c7b9865f5563aede69f2297c2c796b52d9c39f
CodeJedi <34801810+Darsboy@users.noreply.github.com>
2021-01-16 21:24:58 -0500

    Did some More practice

--- Practice Assignment/import.R
+++ Practice Assignment/import.R
@@ -1,2 +1,23 @@
 temp_prec <- read.table("precipitation.txt", header=TRUE)
 temp_prec
+temp_climate <- read.csv("ontarioAll.csv", skip=18, header=TRUE,
+                         na.strings=c("","M","E"))
+temp_climate
+
+cpi <- read.csv("USACPIALLMINMEI.csv")
+names(cpi)[2] <- "CPI"
+
+emp <- read.csv("LNU02300000.csv")
+names(emp)[2] <- "Emp"
+
+gdp <- read.csv("NA000334Q.csv")
+names(gdp)[2] <- "GDP"
+
+
+gdpW <- read.csv("API_NY.GDP.PCAP.PP.CD_DS2_en_csv_v2_656861.csv", skip=4,
+                 header=TRUE)
+
+int <- read.csv("DP_LIVE_15012021222811533.csv")
+
+UnempCan <- read.csv("1410028701-eng (1).csv")
+
